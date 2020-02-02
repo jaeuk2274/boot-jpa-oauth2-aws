2215b329f433b2bc03233399235cdfc3764c6343
jaeuk2274 <jaeuk2274@gmail.com>
2020-02-03 01:37:52 +0900

    ~p116 진행

--- src/test/java/bootJpaAws/web/HelloControllerTest.java
+++ src/test/java/bootJpaAws/web/HelloControllerTest.java
@@ -1,10 +1,13 @@
 package bootJpaAws.web;
 
 
+import org.apache.catalina.security.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,10 +18,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
-    @Autowired private MockMvc mockMvc;
+    @Autowired MockMvc mockMvc;
 
     @Test
     public void 헬로우_리턴() throws Exception {
